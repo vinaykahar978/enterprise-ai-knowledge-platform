eba9f2e716f3466c10bbd2e0752060bf0a29f818
vinay845 <vinaykahar978@gmail.com>
2026-01-20 13:03:03 +0530

    feat: RAG system implementation done

--- app/services/prompt_service.py
+++ app/services/prompt_service.py
@@ -3,7 +3,7 @@ def build_rag_prompt(question: str, contexts: list[str]) -> str:
 
     return f"""
 You are an AI assistant that answers questions strictly using the provided context.
-If the answer is not present in the context, respond with "I don't know".
+If the answer is not present in the context,respond with "I don't know".
 
 Context:
 {context_block}
